9e1480fa1451723d762a0625fac51e16f609e2bb
Oscar Penut <oscar.penut@outlook.com>
2020-10-09 16:23:58 -0600

    Added certificates, cleaned principal page

--- js/dist/mainFunctions.dev.js
+++ js/dist/mainFunctions.dev.js
@@ -1,10 +1,9 @@
+"use strict";
+
 var imgBody = document.getElementById('img-body');
-imgBody.addEventListener('mouseover',
-function changeImage(){
+imgBody.addEventListener('mouseover', function changeImage() {
   imgBody.src = '../img/IMG-20200202-WA0031.jpg';
 });
-imgBody.addEventListener('mouseleave',
-function changeImage(){
+imgBody.addEventListener('mouseleave', function changeImage() {
   imgBody.src = '../img/IMG-20200202-WA0031-upsidedown.jpg';
 });
-
